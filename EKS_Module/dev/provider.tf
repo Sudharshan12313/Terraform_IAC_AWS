8474ba8df4766c9f7ef4ab2e3325041299356f45
Sudharshan12313 <167956857+Sudharshan12313@users.noreply.github.com>
2024-05-02 17:01:41 +0530

    Update provider.tf

--- EKS_Module/dev/provider.tf
+++ EKS_Module/dev/provider.tf
@@ -1,7 +1,14 @@
+#provider "aws" {
+#  region = "${var.aws_region}"
+#  profile = "${var.profile}"
+#}
+
 provider "aws" {
-  region = "${var.aws_region}"
-  profile = "${var.profile}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "${var.aws_region}"
 }
+
 
 
 #terraform {
